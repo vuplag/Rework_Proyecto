1ee6a93b3ce76fa0f91873a78345521bfca16c0f
Rukasu25 <GeniuxProx@gmail.com>
2024-11-24 22:28:06 -0300

    Intento de enlace a la pagina de gameplay principal (Terry)
    no se renderiza como debe

--- Proyecto_intro/urls.py
+++ Proyecto_intro/urls.py
@@ -2,10 +2,10 @@ from django.urls    import path
 from Login.views    import Login
 from Register.views import Register
 from MainPG.views   import Home
-
+from Terry.views   import Terry
 urlpatterns = [
     path('Login/',    Login   ),
     path('Register/', Register), 
     path('',          Home    ),
-
+    path('Terry/',    Terry   ),
 ]
